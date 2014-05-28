2e04ac3efbafd9ee5b5b2d8742bbf6f97dc8777c
HAMA <HAMA@MAK>
2014-05-28 15:19:36 +0200

    good
    Signed-off-by: HAMA <HAMA@MAK>

---
+++ integration-demo/src/test/java/edu/esprit/integration/demo/CalculatorTest.java
@@ -0,0 +1,19 @@
+package edu.esprit.integration.demo;
+
+import static org.junit.Assert.*;
+
+import org.junit.Before;
+import org.junit.Test;
+
+public class CalculatorTest {
+
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
